c2e3b08540ad62ed1b3c434de341369e8387ea5f
vibortolotti <vibortolotti@8123cee4-8aac-11dd-a9db-710176c1da70>
2008-09-25 03:29:50 +0000

    < Definicao das Funções MEPA >

---
+++ trunk/src/Mepa.cpp
@@ -0,0 +1,312 @@
+void
+Mepa::CRCT(int vK)
+{
+	s++;
+	M[s] = vK;
+	proximaInstrucao();
+}
+
+void
+Mepa::CRVL( int k, int n )
+{
+	s++;
+	int a = M[D[k] + n];
+??//	M[s] = a;
+	proximaInstrucao();
+}
+
+void
+Mepa::SOMA()
+{
+	M[s-1] += M[s];
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::SUBT()
+{
+	M[s-1] = M[s-1] - M[s];
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::MULT()
+{
+	M[s-1] *= M[s];
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::DIVI()
+{
+	M[s-1] = M[s-1] / M[s];
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::CMIG()
+{
+	if(M[s-1] == M[s])
+	{
+		M[s-1] = 1;
+	}
+	else
+	{
+		M[s-1] = 0;
+	}
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::CMMA()
+{
+	if ( M[s-1] > M[s] )
+	{
+		M[s-1] = 1;
+	}
+	else
+	{
+		M[s-1] = 0;
+	}
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::CMME()
+{
+	if(M[s-1] < M[s])
+	{
+		M[s-1] = 1;
+	}
+	else
+	{
+		M[s-1] = 0;
+	}
+	s--;
+	proximaInstrucao();
+}
+
+
+void
+Mepa::CMAG()
+{
+	if ( M[s-1] >= M[s] )
+	{
+		M[s-1] = 1;
+	}
+	else
+	{
+		M[s-1] = 0;
+	}
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::CMEG()
+{
+	if(M[s-1] <= M[s])
+	{
+		M[s-1] = 1;
+	}
+	else
+	{
+		M[s-1] = 0;
+	}
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::CMDG()
+{
+	if ( M[s-1] != M[s] )
+	{
+		M[s-1] = 1;
+	}
+	else
+	{
+		M[s-1] = 0;
+	}
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::CONJ()
+{
+	if ( M[s-1] == 1 && M[s] == 1 )
+	{
+		M[s-1] = 1;
+	}
+	else
+	{
+		M[s-1] = 0;
+	}
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::DISJ()
+{
+	if(M[s-1] == 1 || M[s] == 1)
+	{
+		M[s-1] = 1;
+	}
+	else
+	{
+		M[s-1] = 0;
+	}
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::NEGA()
+{
+	M[s] = 1 - M[s];
+	proximaInstrucao();
+}
+
+void
+Mepa::INVR()
+{
+	M[s] = - M[s];
+	proximaInstrucao();
+}
+
+
+void
+Mepa::ARMZ( int k, int n )
+{
+	M[D[k]+n] = M[s];
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::LEIT()
+{
+	s++;
+	std::cin >> M[s];
+	proximaInstrucao();
+}
+
+void
+Mepa::IMPR()
+{
+	std::cout << M[s];
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::NADA()
+{
+	proximaInstrucao();
+}
+
+void
+Mepa::DSVS( int p )
+{
+	proximaInstrucao( p );
+}
+
+void
+Mepa::DSVF( int p )
+{
+	if(M[s] == 0)
+	{
+		proximaInstrucao( p );
+	}
+	else
+	{
+		proximaInstrucao();
+	}
+	s--;
+}
+
+void
+Mepa::INPP()
+{
+	this->s = -1;
+	D[0] = 0;
+	proximaInstrucao();
+}
+
+void
+Mepa::AMEM( int m )
+{
+	s += m;
+	proximaInstrucao();
+}
+
+void
+Mepa::DMEM( int n )
+{
+	s -= n;
+	proximaInstrucao();
+}
+
+void
+Mepa::PARA()
+{
+
+}
+
+void
+Mepa::CHPR( int L )
+{
+	s++;
+	M[s] = PC + 1;
+	PC = L;
+}
+
+void
+Mepa::ENPR( int k )
+{
+	s++;
+	M[s] = D[k];
+	D[k] = s + 1;
+	proximaInstrucao();
+}
+
+void
+Mepa::RTPR( int k, int n )
+{
+	D[k] = M[s];
+	PC = M[s-1];
+	s -= (n+2);
+}
+
+
+void
+Mepa::CRVI( int k, int n )
+{
+	s++;
+	M[s] = M[ M[D[k] + n] ];
+	proximaInstrucao();
+}
+
+void
+Mepa::ARMI( int k, int n )
+{
+	M[ M[D[k]+n] ] = M[s];
+	s--;
+	proximaInstrucao();
+}
+
+void
+Mepa::CREN( int k, int n )
+{
+	s++;
+	M[s] = D[k] + n;
+	proximaInstrucao();
+}
